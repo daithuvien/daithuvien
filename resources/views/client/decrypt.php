646c09da39ff3b6c80519274e0b2cd05911225b3
leminhy89 <leminhy89@gmail.com>
2023-03-02 15:13:06 +0700

    Update decrypt.php

--- resources/views/client/decrypt.php
+++ resources/views/client/decrypt.php
@@ -10,112 +10,249 @@ function encrypt_dtv($textToEncrypt, $base64Key) {
     return base64_encode($iv.$ciphertext.$tag);
 }
 
+function decrypt($base64IvCiphertext, $base64Key) {
+    $ivCiphertext = base64_decode($base64IvCiphertext);
+    $key = base64_decode($base64Key);
+    $method = 'aes-256-gcm';
+    $iv = substr($ivCiphertext, 0, 12);
+    $tag = substr($ivCiphertext, -16);
+    $ciphertext = substr($ivCiphertext, 12, -16);
+
+    $decrypted = openssl_decrypt($ciphertext, $method, $key, OPENSSL_RAW_DATA, $iv, $tag);
+    if($decrypted === FALSE) {
+        throw new Exception('Can\'t decrypt, bad data.');
+    }
+
+    return $decrypted;
+}
+
+$KEY = 'MOJPGc8Kq7bG+YhgwrTowXQ5dYq598WgEBiESun0fsM=';
+
+function encode($text) {
+    global $KEY;
+    return encrypt_dtv($text, $KEY);
+}
+
+function decode($encoded) {
+    global $KEY;
+    return decrypt($encoded, $KEY);
+}
+
+/* END OF DECRYPT FUNCTIONS */
+
+$VERSION = 4;
+
 function fail($reason, $status = 400) {
+    global $VERSION;
     http_response_code($status);
-    echo json_encode(array('status' => 'error', 'reason' => $reason));
+    echo json_encode(array('status' => 'error', 'reason' => $reason, 'version' => $VERSION), JSON_UNESCAPED_SLASHES);
     exit;
 }
 
 function success($data, $status = 200) {
+    global $VERSION;
     http_response_code($status);
-    echo json_encode(array('status' => 'ok', 'data' => $data));
+    echo json_encode(array('status' => 'ok', 'data' => $data, 'version' => $VERSION), JSON_UNESCAPED_SLASHES);
     exit;
 }
 
+function escapeRegEx($literal) {
+    return preg_quote($literal, '/');
+}
+
+function censorSensitiveData($text, $sensitive) {
+    $escaped = join('|', array_map('escapeRegEx', $sensitive));
+    return preg_replace('/(?:' . $escaped . ')/', '[censored]', $text);
+}
+
+function apiRequest($auth, $path, $sensitive, $options=array()) {
+    $headersDict = isset($options['headers']) ? $options['headers'] : array();
+    if(!isset($headersDict['Authorization'])) {
+        $headersDict['Authorization'] = 'Bearer ' . $auth;
+    }
+    if(!isset($headersDict['Accept'])) {
+        $headersDict['Accept'] = 'application/json';
+    }
+    $headers = array();
+    foreach($headersDict as $headerName => $headerValue) {
+        array_push($headers, $headerName . ': ' . $headerValue);
+    }
+
+    $ch = curl_init('https://www.googleapis.com/drive/v3/' . $path);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    if(isset($options['body'])) {
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $options['body']);
+    }
+
+    $text = curl_exec($ch);
+    curl_close($ch);
+
+    $responseData = json_decode($text, TRUE);
+    if($responseData === NULL) {
+        throw new Exception(censorSensitiveData($text, $sensitive));
+    }
+
+    if(isset($responseData['error'])) {
+        throw new Exception(censorSensitiveData($responseData['error']['message'], $sensitive));
+    }
+
+    return $responseData;
+}
+
+$info = function($data) {
+    $folderId = decode($data['folder']);
+
+    $folderInfo = apiRequest(
+        $data['auth'],
+        'files/' . $folderId . '?supportsAllDrives=true&fields=name,mimeType,shortcutDetails/*',
+        array($folderId)
+    );
+
+    // if it's a folder, grab the contents
+    if($folderInfo['mimeType'] === 'application/vnd.google-apps.folder') {
+        $folderContents = apiRequest(
+            $data['auth'],
+            'files?q="' . $folderId . '"+in+parents'
+            . '&fields=nextPageToken,files(id,size,name,mimeType,md5Checksum,shortcutDetails/*)'
+            . '&orderBy=name_natural&supportsAllDrives=true&includeItemsFromAllDrives=true&pageSize=100'
+            . (isset($data['pageToken']) ? '&pageToken=' . $data['pageToken'] : ''),
+            array($folderId)
+        );
+    }
+    // if it's shortcut/file, set notLoaded to true and grab the info later
+    elseif($folderInfo['mimeType'] === 'application/vnd.google-apps.shortcut') {
+        $folderContents = array(
+            'files' => array(array(
+                'notLoaded' => TRUE,
+                'id' => $folderInfo['shortcutDetails']['targetId'],
+                'mimeType' => $folderInfo['shortcutDetails']['targetMimeType'],
+                'name' => $folderInfo['name']
+            ))
+        );
+        unset($folderInfo['shortcutDetails']);
+    }
+    else {
+        $folderContents = array(
+            'files' => array(array(
+                'notLoaded' => TRUE,
+                'id' => $folderId,
+                'mimeType' => $folderInfo['mimeType'],
+                'name' => $folderInfo['name']
+            ))
+        );
+        unset($folderInfo['shortcutDetails']);
+    }
+    unset($folderInfo['mimeType']);
+
+    $files = array();
+    foreach($folderContents['files'] as $file) {
+        if($file['mimeType'] === 'application/vnd.google-apps.shortcut') {
+            $file['notLoaded'] = TRUE;
+            $file['id'] = $file['shortcutDetails']['targetId'];
+            $file['mimeType'] = $file['shortcutDetails']['targetMimeType'];
+        }
+
+        if(isset($file['notLoaded']) && $file['notLoaded'] === TRUE) {
+            // ignore shortcuts to folders
+            if($file['mimeType'] === 'application/vnd.google-apps.folder') {
+                continue;
+            }
+            $fileInfo = apiRequest(
+                $data['auth'],
+                'files/' . $file['id'] . '?supportsAllDrives=true&fields=size,md5Checksum',
+                array($file['id'])
+            );
+            $fileInfo['id'] = $file['id'];
+            $fileInfo['mimeType'] = $file['mimeType'];
+            $fileInfo['name'] = $file['name'];
+        }
+        else {
+            $fileInfo = $file;
+        }
+
+        $fileInfo['id'] = encode($fileInfo['id']);
+        array_push($files, $fileInfo);
+    }
+
+    $folderContents['files'] = $files;
+
+    return array_merge($folderContents, $folderInfo);
+};
+
+function cloneOne($auth, $fileId, $folder) {
+    return apiRequest(
+        $auth,
+        'files/' . $fileId . '/copy?supportsAllDrives=true',
+        array($fileId),
+        array(
+            'headers' => array(
+                'Content-Type' => 'application/json'
+            ),
+            'body' => json_encode(array(
+                'parents' => array($folder),
+                'appProperties' => array(
+                    'createdWithDdEfc' => 1
+                )
+            ), JSON_UNESCAPED_SLASHES)
+        )
+    );
+}
+
+$clone = function($data) {
+    $result = array();
+    foreach($data['files'] as $id) {
+        $realId = decode($id);
+        $cloneResult = cloneOne($data['auth'], $realId, $data['destination']);
+        array_push($result, array(
+            'id' => $id,
+            'data' => $cloneResult
+        ));
+    }
+
+    return $result;
+};
+
 $method = $_SERVER['REQUEST_METHOD'];
 
-if($method === 'POST') {
-    header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
 
-    $requestData = json_decode(file_get_contents('php://input'), TRUE);
-    if($requestData === NULL) {
-        fail('Invalid json data');
-    }
+if($method === 'OPTIONS') {
+    exit;
+}
 
-    try {
-        success(encrypt_dtv($requestData['folder'], 'MOJPGc8Kq7bG+YhgwrTowXQ5dYq598WgEBiESun0fsM='));
-    }
-    catch(Exception $e) {
-        fail($e.getMessage());
-    }
+header('Content-Type: application/json');
+
+if($method !== 'POST') {
+    fail('Method not allowed', 405);
+}
+
+$page = isset($_GET['page']) ? $_GET['page'] : '';
+
+if($page === 'info') {
+    $parser = $info;
+}
+elseif($page === 'clone') {
+    $parser = $clone;
+}
+else {
+    fail('Page not found', 404);
+}
+
+$requestData = json_decode(file_get_contents('php://input'), TRUE);
+if($requestData === NULL) {
+    fail('Invalid json data');
+}
+
+try {
+    success($parser($requestData));
+}
+catch(Exception $e) {
+    fail($e->getMessage());
 }
 
 ?>
-
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <title>Google Drive - encrypted folder copy</title>
-</head>
-<body>
-  <div class="container"><div class="row"><div class="col">
-    <div class="card my-4">
-      <div class="card-body">
-        <h3>Encrypt link</h3>
-        <div class="form-group form-row align-items-center">
-          <div class="col">
-            <input id="folder_link" type="text" class="form-control" placeholder="Google Drive folder link">
-          </div>
-          <div class="col-3 col-md-2">
-            <button id="encrypt" class="btn btn-primary btn-block">Encrypt</button>
-          </div>
-        </div>
-        <input id="encrypted_link" type="text" class="form-control" placeholder="encrypted ID">
-        <p class="mt-3">You can share the decryption website and encrypted ID separately or combine them together with <code>#</code> like this: <code>https://example.com/#ABCD.1234</code></p>
-      </div>
-      
-    </div>
-  </div></div></div>
-
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  <script type="text/javascript">
-
-async function encryptId(folderId) {
-  const result = await fetch("encrypt", {method: "POST", body: JSON.stringify({folder: folderId})});
-  const data = await result.json();
-  if(result.ok) {
-    return data.data;
-  }
-  else {
-    throw Error(data.reason);
-  }
-}
-
-async function encryptFolder() {
-  const link = document.querySelector("#folder_link").value;
-  const m = link.match(/^https:\/\/drive\.google\.com\/(?:folderview\?id=|open\?id=|drive\/(?:u\/\d+\/)?folders\/|file\/(?:u\/\d+\/)?d\/|uc\?id=)([0-9a-zA-Z\-_]+)/);
-  if(m === null) {
-    throw Error("bad link!");
-  }
-
-  return encryptId(m[1]);
-}
-
-document.querySelector("#encrypt").addEventListener("click", async function(event) {
-  event.preventDefault();
-
-  document.querySelector("#folder_link").setAttribute("disabled", "");
-  document.querySelector("#encrypt").setAttribute("disabled", "");
-  try {
-    document.querySelector("#encrypted_link").value = "ZGFpdGh1dmllbi5jb20vZGVjcnlwdA==." + (await encryptFolder());
-  }
-  catch(e) {
-    alert(e.message);
-  }
-  finally {
-    document.querySelector("#folder_link").removeAttribute("disabled");
-    document.querySelector("#encrypt").removeAttribute("disabled");
-  }
-});
-
-  </script>
-</body>
-</html>
-
