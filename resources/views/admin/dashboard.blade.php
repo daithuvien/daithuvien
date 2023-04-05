236a2fded88d0ff7ecaf0efb0ed25a388b1293a0
yleminh <leminhy89@gmail.com>
2023-04-05 11:06:58 +0700

    update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -12,22 +12,22 @@
           <div class="col-span-12 mt-8">
               <div class="intro-y flex items-center h-10">
                   <h2 class="text-lg font-medium truncate mr-5">
-                      General Report
+                      Báo Cáo Chung
                   </h2>
-                  <a href="" class="ml-auto flex items-center text-primary"> <i data-lucide="refresh-ccw" class="w-4 h-4 mr-3"></i> Reload Data </a>
+                  <a href="" class="ml-auto flex items-center text-primary"> <i data-lucide="refresh-ccw" class="w-4 h-4 mr-3"></i> Làm mới Data </a>
               </div>
               <div class="grid grid-cols-12 gap-6 mt-5">
                   <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
                       <div class="report-box zoom-in">
                           <div class="box p-5">
                               <div class="flex">
-                                  <i data-lucide="shopping-cart" class="report-box__icon text-primary"></i> 
+                                  <i data-lucide="user" class="report-box__icon text-primary"></i> 
                                   <div class="ml-auto">
                                       <div class="report-box__indicator bg-success tooltip cursor-pointer" title="33% Higher than last month"> 33% <i data-lucide="chevron-up" class="w-4 h-4 ml-0.5"></i> </div>
                                   </div>
                               </div>
                               <div class="text-3xl font-medium leading-8 mt-6">4.710</div>
-                              <div class="text-base text-slate-500 mt-1">Item Sales</div>
+                              <div class="text-base text-slate-500 mt-1">Người dùng</div>
                           </div>
                       </div>
                   </div>
@@ -40,8 +40,8 @@
                                       <div class="report-box__indicator bg-danger tooltip cursor-pointer" title="2% Lower than last month"> 2% <i data-lucide="chevron-down" class="w-4 h-4 ml-0.5"></i> </div>
                                   </div>
                               </div>
-                              <div class="text-3xl font-medium leading-8 mt-6">3.721</div>
-                              <div class="text-base text-slate-500 mt-1">New Orders</div>
+                              <div class="text-3xl font-medium leading-8 mt-6">2</div>
+                              <div class="text-base text-slate-500 mt-1">Khóa học đã release</div>
                           </div>
                       </div>
                   </div>
@@ -54,8 +54,8 @@
                                       <div class="report-box__indicator bg-success tooltip cursor-pointer" title="12% Higher than last month"> 12% <i data-lucide="chevron-up" class="w-4 h-4 ml-0.5"></i> </div>
                                   </div>
                               </div>
-                              <div class="text-3xl font-medium leading-8 mt-6">2.149</div>
-                              <div class="text-base text-slate-500 mt-1">Total Products</div>
+                              <div class="text-3xl font-medium leading-8 mt-6">185</div>
+                              <div class="text-base text-slate-500 mt-1">Tổng khóa học</div>
                           </div>
                       </div>
                   </div>
@@ -69,7 +69,7 @@
                                   </div>
                               </div>
                               <div class="text-3xl font-medium leading-8 mt-6">152.040</div>
-                              <div class="text-base text-slate-500 mt-1">Unique Visitor</div>
+                              <div class="text-base text-slate-500 mt-1"> Lượt truy cập</div>
                           </div>
                       </div>
                   </div>
@@ -80,7 +80,7 @@
           <div class="col-span-12 lg:col-span-6 mt-8">
               <div class="intro-y block sm:flex items-center h-10">
                   <h2 class="text-lg font-medium truncate mr-5">
-                      Sales Report
+                      Lượng Truy Cập
                   </h2>
                   <div class="sm:ml-auto mt-3 sm:mt-0 relative text-slate-500">
                       <i data-lucide="calendar" class="w-4 h-4 z-10 absolute my-auto inset-y-0 ml-3 left-0"></i> 
@@ -91,12 +91,12 @@
                   <div class="flex flex-col md:flex-row md:items-center">
                       <div class="flex">
                           <div>
-                              <div class="text-primary dark:text-slate-300 text-lg xl:text-xl font-medium">$15,000</div>
+                              <div class="text-primary dark:text-slate-300 text-lg xl:text-xl font-medium">15,000</div>
                               <div class="mt-0.5 text-slate-500">This Month</div>
                           </div>
                           <div class="w-px h-12 border border-r border-dashed border-slate-200 dark:border-darkmode-300 mx-4 xl:mx-5"></div>
                           <div>
-                              <div class="text-slate-500 text-lg xl:text-xl font-medium">$10,000</div>
+                              <div class="text-slate-500 text-lg xl:text-xl font-medium">10,000</div>
                               <div class="mt-0.5 text-slate-500">Last Month</div>
                           </div>
                       </div>
@@ -125,7 +125,7 @@
           <div class="col-span-12 sm:col-span-6 lg:col-span-3 mt-8">
               <div class="intro-y flex items-center h-10">
                   <h2 class="text-lg font-medium truncate mr-5">
-                      Weekly Top Seller
+                      Mục Truy Cập
                   </h2>
                   <a href="" class="ml-auto text-primary truncate">Show More</a> 
               </div>
@@ -138,15 +138,15 @@
                   <div class="w-52 sm:w-auto mx-auto mt-8">
                       <div class="flex items-center">
                           <div class="w-2 h-2 bg-primary rounded-full mr-3"></div>
-                          <span class="truncate">17 - 30 Years old</span> <span class="font-medium ml-auto">62%</span> 
+                          <span class="truncate">Khóa Học</span> <span class="font-medium ml-auto">62%</span> 
                       </div>
                       <div class="flex items-center mt-4">
                           <div class="w-2 h-2 bg-pending rounded-full mr-3"></div>
-                          <span class="truncate">31 - 50 Years old</span> <span class="font-medium ml-auto">33%</span> 
+                          <span class="truncate">Phim</span> <span class="font-medium ml-auto">33%</span> 
                       </div>
                       <div class="flex items-center mt-4">
                           <div class="w-2 h-2 bg-warning rounded-full mr-3"></div>
-                          <span class="truncate">>= 50 Years old</span> <span class="font-medium ml-auto">10%</span> 
+                          <span class="truncate">Truyện Chử</span> <span class="font-medium ml-auto">10%</span> 
                       </div>
                   </div>
               </div>
@@ -156,7 +156,7 @@
           <div class="col-span-12 sm:col-span-6 lg:col-span-3 mt-8">
               <div class="intro-y flex items-center h-10">
                   <h2 class="text-lg font-medium truncate mr-5">
-                      Sales Report
+                      Trình Duyệt
                   </h2>
                   <a href="" class="ml-auto text-primary truncate">Show More</a> 
               </div>
@@ -169,15 +169,15 @@
                   <div class="w-52 sm:w-auto mx-auto mt-8">
                       <div class="flex items-center">
                           <div class="w-2 h-2 bg-primary rounded-full mr-3"></div>
-                          <span class="truncate">17 - 30 Years old</span> <span class="font-medium ml-auto">62%</span> 
+                          <span class="truncate">Chrome</span> <span class="font-medium ml-auto">62%</span> 
                       </div>
                       <div class="flex items-center mt-4">
                           <div class="w-2 h-2 bg-pending rounded-full mr-3"></div>
-                          <span class="truncate">31 - 50 Years old</span> <span class="font-medium ml-auto">33%</span> 
+                          <span class="truncate">Cốc Cốc</span> <span class="font-medium ml-auto">33%</span> 
                       </div>
                       <div class="flex items-center mt-4">
                           <div class="w-2 h-2 bg-warning rounded-full mr-3"></div>
-                          <span class="truncate">>= 50 Years old</span> <span class="font-medium ml-auto">10%</span> 
+                          <span class="truncate">Khác</span> <span class="font-medium ml-auto">10%</span> 
                       </div>
                   </div>
               </div>
@@ -504,7 +504,7 @@
               <div class="col-span-12 md:col-span-6 xl:col-span-4 2xl:col-span-12 mt-3 2xl:mt-8">
                   <div class="intro-x flex items-center h-10">
                       <h2 class="text-lg font-medium truncate mr-5">
-                          Transactions
+                          Truy Cập Mới Nhất
                       </h2>
                   </div>
                   <div class="mt-5">
@@ -517,7 +517,7 @@
                                   <div class="font-medium">Russell Crowe</div>
                                   <div class="text-slate-500 text-xs mt-0.5">29 April 2022</div>
                               </div>
-                              <div class="text-danger">-$81</div>
+                              <div class="text-danger">-81</div>
                           </div>
                       </div>
                       <div class="intro-x">
@@ -529,7 +529,7 @@
                                   <div class="font-medium">Denzel Washington</div>
                                   <div class="text-slate-500 text-xs mt-0.5">16 August 2022</div>
                               </div>
-                              <div class="text-success">+$116</div>
+                              <div class="text-success">+116</div>
                           </div>
                       </div>
                       <div class="intro-x">
@@ -541,7 +541,7 @@
                                   <div class="font-medium">Kevin Spacey</div>
                                   <div class="text-slate-500 text-xs mt-0.5">18 June 2020</div>
                               </div>
-                              <div class="text-success">+$141</div>
+                              <div class="text-success">+141</div>
                           </div>
                       </div>
                       <div class="intro-x">
@@ -553,7 +553,7 @@
                                   <div class="font-medium">Leonardo DiCaprio</div>
                                   <div class="text-slate-500 text-xs mt-0.5">21 December 2022</div>
                               </div>
-                              <div class="text-success">+$177</div>
+                              <div class="text-success">+177</div>
                           </div>
                       </div>
                       <div class="intro-x">
@@ -565,7 +565,7 @@
                                   <div class="font-medium">Kevin Spacey</div>
                                   <div class="text-slate-500 text-xs mt-0.5">25 July 2022</div>
                               </div>
-                              <div class="text-success">+$44</div>
+                              <div class="text-success">+44</div>
                           </div>
                       </div>
                       <a href="" class="intro-x w-full block text-center rounded-md py-3 border border-dotted border-slate-400 dark:border-darkmode-300 text-slate-500">View More</a> 
@@ -576,7 +576,7 @@
               <div class="col-span-12 md:col-span-6 xl:col-span-4 2xl:col-span-12 mt-3">
                   <div class="intro-x flex items-center h-10">
                       <h2 class="text-lg font-medium truncate mr-5">
-                          Recent Activities
+                          Hoạt động gần đây
                       </h2>
                       <a href="" class="ml-auto text-primary truncate">Show More</a> 
                   </div>
